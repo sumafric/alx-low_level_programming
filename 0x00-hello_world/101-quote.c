80019e0355e246a6f1c24b9b2e12b0402354dace
Abel Sum <abelsum1330@gmail.com>
2024-06-20 11:33:44 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n";
+	ssize_t ret = write(2, text, sizeof(text) - 1);
+
+	if (ret != sizeof(text) - 1)
+	{
+		return (0);
+	}
+	return (1);
+}
