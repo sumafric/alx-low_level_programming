01cced627ff35318e492f518adfe54d64d9d7b53
Abel Sum <abelsum1330@gmail.com>
2024-06-23 00:50:31 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	a = '0';
+	b = '9';
+	c = ',';
+	d = ' ';
+	while (a <= b)
+	{
+		if (a < b)
+		{
+			putchar(a);
+			putchar(c);
+		}
+		else
+		{
+			putchar(a);
+		}
+		putchar(d);
+		a++;
+	}
+	return (0);
+}
