8b194c97e6519f8c490091018d9922704e591834
Abel Sum <abelsum1330@gmail.com>
2024-06-25 09:56:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+	char b[] = "_putchar";
+
+	for (a = 0; a < 8; a++)
+	{
+		_putchar(b[i]);
+	}
+		_putchar('\n');
+	return (0);
+}
