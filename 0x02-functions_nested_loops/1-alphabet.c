699971517e76570a68249ed24ad4f8f288ce6310
Abel Sum <abelsum1330@gmail.com>
2024-06-25 15:56:01 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Description: print_alphabet - make the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+}
