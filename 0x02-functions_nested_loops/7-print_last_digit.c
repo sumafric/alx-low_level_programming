57a655344b88e14226267cfec64a6a1ee8935cc2
Abel Sum <abelsum1330@gmail.com>
2024-06-26 06:37:11 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of a given integer
+ * @num: The integer
+ *
+ * Return: Last digit of int
+ */
+int print_last_digit(int num)
+{
+	int lastDigit;
+
+	if (num < 0)
+	{
+		num = -num;
+	}
+	lastDigit = num % 10;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
