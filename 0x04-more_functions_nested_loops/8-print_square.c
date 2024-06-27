c882d3238296a0470f4343de89e8011a2c50a4aa
Abel Sum <abelsum1330@gmail.com>
2024-06-27 16:47:54 -0700

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints square of num n times
+ * @size: n times
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
