b2da7fed3709464f2f6cf0349767c9fbcc812037
Abel Sum <abelsum1330@gmail.com>
2024-07-07 04:39:20 -0700

    The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,9 @@
  */
 void print_line(int n)
 {
-	int a;
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (a = 0; a < n; a++)
-	{
-	_putchar(95);
-	}
-	_putchar('\n');
-	}
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
