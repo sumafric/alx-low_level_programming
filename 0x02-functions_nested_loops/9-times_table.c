f29239461169f6ec374efef139e42f3ac0fde479
Abel Sum <abelsum1330@gmail.com>
2024-07-07 03:20:54 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * times_table - prints the timetable nine times
- * Return: empty output
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
