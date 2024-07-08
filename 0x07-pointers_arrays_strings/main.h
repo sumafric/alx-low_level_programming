0cc124d45fc115314c7e7039ae00c79badb510b6
Abel Sum <abelsum1330@gmail.com>
2024-07-08 11:20:42 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Dev Nderitu
- *description: Header file containing prototypes for all functions
- * used in the0x07-pointers_arrays_strings directory
- */
-
 
 #include <stdio.h>
 #include <stddef.h>
