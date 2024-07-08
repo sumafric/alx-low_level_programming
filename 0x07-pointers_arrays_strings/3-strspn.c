41ddb99b37dc13be62bd4a76ac52de1ee323983d
Abel Sum <abelsum1330@gmail.com>
2024-07-08 11:39:29 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strspn - search the number of bytes in the initial segment of s
+ *@s:segment targeted
+ *@accept:reference bytes container
+ *Return: the number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+			{
+				bytes++;
+				break;
+			}
+			else if ((accept[i + 1]) == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
