17d06fbd4216bedc657810100609c7ad5b2d3184
Abel Sum <abelsum1330@gmail.com>
2024-07-11 08:05:38 -0700

    Inception

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,25 +8,11 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-	else if (*s2 == '*')
-	{
-		return (wildcmp(s1, s2 + 1));
-	}
-	else if (*s1 != '\0' && wildcmp(s1 + 1, s2))
-	{
-		return (1);
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	else
-	{
-		return (0);
-	}
-
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
 }
