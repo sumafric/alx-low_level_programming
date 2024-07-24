a8bcbd10be8ecfec70a33080e0f0536630d8f547
Abel Sum <abelsum1330@gmail.com>
2024-07-23 17:47:09 -0700

    New_dog correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,66 +1,49 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-/**
- * _strlen - Length of the string
- * @str: String
- *
- * Return: Length
- */
-int _strlen(char *str)
-{
-	if (*str == '\0')
-	{
-		return (0);
-	}
-	return (1 + _strlen(str + 1));
-}
-/**
- * _strdup - Duplicates a string
- * @str: String to duplicate
- *
- * Return: Duplicate
- */
-char *_strdup(char *str)
-{
-	int i, len;
-	char *newStr;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	len = _strlen(str);
-	newStr = malloc(sizeof(char) * len);
-	if (newStr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0;  i < len; i++)
-	{
-		newStr[i] = str[i];
-	}
-	return (newStr);
-}
 /**
- * new_dog - Creates a new_dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- *
- * Return: The new dog
- */
+* new_dog - function that creates a new dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: the pointer to the dog
+**/
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog;
+	dog_t *_dog;
+	int i, j, l, k;
 
-	newDog = malloc(sizeof(dog_t));
-	if (newDog == NULL)
+	i = j = 0;
+	_dog = malloc(sizeof(dog_t));
+	if (_dog == NULL)
+		return (NULL);
+	if (name != NULL)
+		while (name[i] != '\0')
+			i += 1;
+	if (owner != NULL)
+		while (owner[j] != '\0')
+			j += 1;
+	_dog->name = malloc(i * sizeof(char) + 1);
+	if (_dog->name == NULL)
 	{
+		free(_dog);
 		return (NULL);
 	}
-	newDog->name = _strdup(name);
-	newDog->age = age;
-	newDog->owner = _strdup(owner);
-
-	return (newDog);
+	_dog->owner = malloc(j * sizeof(char) + 1);
+	if (_dog->owner == NULL)
+	{
+		free(_dog->name);
+		free(_dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k += 1)
+		_dog->name[k] = name[k];
+	for (l = 0; l < j; l += 1)
+		_dog->owner[l] = owner[l];
+	_dog->name[k] = '\0';
+	_dog->owner[l] = '\0';
+	_dog->age = age;
+	return (_dog);
 }
