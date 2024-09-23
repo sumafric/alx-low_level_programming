3b6abba01aeeb12082cd2f17986db7537eba1ee4
Abel Sum <abelsum1330@gmail.com>
2024-09-23 07:02:12 -0700

    Lets call C functions from Python

---
+++ 0x18-dynamic_libraries/100-tests.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adding int a and int b
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * mul - multiplying int a and int b
+ * @a: first int
+ * @b: second int
+ * Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - dividing int a and int b
+ * @a: first int
+ * @b: second int
+ * Return: Quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * sub - subtracting int a and int b
+ * @a: first int
+ * @b: second int
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mod - remainder of a % b
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
